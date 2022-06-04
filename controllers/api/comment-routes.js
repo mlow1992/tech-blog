0743eee6869add52a6c3bc71fbcee6858d3ae016
General Zelgius <mattlow123@gmail.com>
2022-06-04 18:32:32 -0400

    added styling and idle logout functionality after 30 minutes

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,58 @@
+const router = require('express').Router();
+const withAuth = require('../../utils/auth');
+const { Comment, User } = require('../../models');
+
+router.get('/', (req, res) => {
+    Comment.findAll({
+        attributes: ['id', 'comment_text', 'created_at'],
+        order: [['created_at', 'DESC']],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.post('/', withAuth, (req, res) => {
+    if (req.session) {
+        Comment.create({
+            comment_text: req.body.comment_text,
+            // use the id from the session
+            user_id: req.session.user_id,
+            post_id: req.body.post_id
+        })
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    }
+});
+
+router.delete('/:id', withAuth, (req, res) => {
+    Comment.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbCommentData => {
+        if (!dbCommentData) {
+            res.status(404).json({ message: 'No comment found with this id' });
+            return;
+        }
+        res.json(dbCommentData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
